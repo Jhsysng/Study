16862f80d734554304995790741d2c640ff348e4
alx <ysng1118@naver.com>
2022-07-31 15:27:14 +0900

    ?post detail pagetest

--- Django/blog/tests.py
+++ Django/blog/tests.py
@@ -2,7 +2,6 @@ from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from .models import Post
 class TesstView(TestCase):
-
     def setUp(self):
         self.client=Client()
     def test_post_list(self):
@@ -39,4 +38,31 @@ class TesstView(TestCase):
         self.assertIn(post__002.title,main_area.text)
 
         self.assertNotIn('아직 게시물 없음',main_area.text)
+
+        def test_post_detail(self):
+            post_001=Post.objects.create(
+                title='1st 포스트',
+                content='hihiihihim going library later'
+            )
+
+            self.assertEqual(post_001.get_absolute_url(),'/blog/1/')
+
+            response=self.client.get(post_001.get_absolute_url())
+            self.assertEqual(response.status_code,200)
+            soup=BeautifulSoup(response.content,'html.parser')
+
+            navbar=soup.nav
+            self.assertIn('Blog',navbar.text)
+            self.assertIn('About me',navbar.text)
+
+            self.assetIn(post_001.title, soup.title.text)
+
+            main_area=soup.find('div',id='main-area')
+            post_area=soup.find('div',id='post-area')
+            self.asserIn(post_001.title,post_area.text)
+
+            self.assertIn(post_001.content,post_area.text)
+
+
+
 # Create your tests here.
