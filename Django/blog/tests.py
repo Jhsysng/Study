f00288cb7eee88c07a8a455894b41324dc84335e
alx <ysng1118@naver.com>
2022-08-01 14:31:05 +0900

    postlist&postdetail module

--- Django/blog/tests.py
+++ Django/blog/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from .models import Post
-class TesstView(TestCase):
+class TestView(TestCase):
     def setUp(self):
         self.client=Client()
     def test_post_list(self):
@@ -39,29 +39,29 @@ class TesstView(TestCase):
 
         self.assertNotIn('아직 게시물 없음',main_area.text)
 
-        def test_post_detail(self):
-            post_001=Post.objects.create(
-                title='1st 포스트',
-                content='hihiihihim going library later'
-            )
+    def test_post_detail(self):
+        post_001=Post.objects.create(
+            title='1st 포스트',
+            content='hihiihihim going library later'
+        )
 
-            self.assertEqual(post_001.get_absolute_url(),'/blog/1/')
+        self.assertEqual(post_001.get_absolute_url(),'/blog/1/')
 
-            response=self.client.get(post_001.get_absolute_url())
-            self.assertEqual(response.status_code,200)
-            soup=BeautifulSoup(response.content,'html.parser')
+        response=self.client.get(post_001.get_absolute_url())
+        self.assertEqual(response.status_code,200)
+        soup=BeautifulSoup(response.content,'html.parser')
 
-            navbar=soup.nav
-            self.assertIn('Blog',navbar.text)
-            self.assertIn('About me',navbar.text)
+        navbar=soup.nav
+        self.assertIn('Blog',navbar.text)
+        self.assertIn('About me',navbar.text)
 
-            self.assetIn(post_001.title, soup.title.text)
+        self.assertIn(post_001.title, soup.title.text)
 
-            main_area=soup.find('div',id='main-area')
-            post_area=soup.find('div',id='post-area')
-            self.asserIn(post_001.title,post_area.text)
+        main_area=soup.find('div',id='main-area')
+        post_area=soup.find('div',id='post-area')
+        self.assertIn(post_001.title,post_area.text)
 
-            self.assertIn(post_001.content,post_area.text)
+        self.assertIn(post_001.content,post_area.text)
 
 
 
