1e035d8af1f74255e85787b5e7f64a7fb5cc1ac5
alx <ysng1118@naver.com>
2022-08-03 17:30:31 +0900

    post category

--- Django/blog/models.py
+++ Django/blog/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 import os
+
+class Category(models.Model):
+    name=models.CharField(max_length=50,unique=True)
+    slug=models.SlugField(max_length=200,unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 class Post(models.Model):
     title=models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
@@ -12,6 +22,8 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    category = models.ForeignKey(Category, null=True,blank=True, on_delete=models.SET_NULL)
 
 
     def __str__(self):
@@ -25,4 +37,6 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+
+
 # Create your models here.
