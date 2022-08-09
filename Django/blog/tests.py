941d8ae22256347736c3f70c9acbb14a9342ecd3
alx <ysng1118@naver.com>
2022-08-09 15:01:20 +0900

    post tag

--- Django/blog/tests.py
+++ Django/blog/tests.py
@@ -107,16 +107,25 @@ class TestView(TestCase):
         main_area=soup.find('div',id='main-area')
         self.assertIn('Create New Post',main_area.text)
 
+        tag_str_input=main_area.find('input',id='id_tags_str')
+        self.assertTrue(tag_str_input)
+
         self.client.post(
             '/blog/create_post/',
             {
                 'title':'post form',
                 'content':"post form page",
+                'tags_str':'new_tag; 한글태그, python'
             }
         )
         last_post=Post.objects.last()
         self.assertEqual(last_post.title,"post form")
         self.assertEqual(last_post.author.username,'obama')
+
+        self.assertEqual(last_post.tags.count(),3)
+        self.assertTrue(Tag.objects.get(name='new_tag'))
+        self.assertTrue(Tag.objects.get(name='한글태그'))
+        self.assertEqual(Tag.objects.count(),5)
     def test_tag_page(self):
         response=self.client.get(self.tag_hello.get_absolute_url())
         self.assertEqual(response.status_code,200)
