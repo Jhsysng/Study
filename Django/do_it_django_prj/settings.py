00338b59707194de6a38db92e3a44b3e7195d54b
alx <ysng1118@naver.com>
2022-08-09 16:23:30 +0900

    django-crispy

--- Django/do_it_django_prj/settings.py
+++ Django/do_it_django_prj/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'blog',
     'single_pages',
     'django_extensions',
+
+    'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
@@ -122,6 +125,7 @@ STATIC_URL = 'static/'
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'_media')
+CRISPY_TEMPLATE_PACK='bootstrap4'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
