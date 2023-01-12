679a20069b8e8a8dfa999817a2931a18af3f4a4b
Jhsysng <ysng1118@naver.com>
2023-01-13 00:03:28 +0900

    API swagger, log add

---
+++ Spring/SpringJPAtest/src/main/java/com/springboot/jpa/SpringJpAtestApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpAtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpAtestApplication.class, args);
+    }
+
+}
