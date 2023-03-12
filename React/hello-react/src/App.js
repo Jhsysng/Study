4725432dee5d7c7d27429be096a37416319f5968
Jhsysng <screenshot161@gmail.com>
2023-03-12 15:27:00 +0900

    react

---
+++ React/hello-react/src/App.js
@@ -0,0 +1,10 @@
+import Say from './Say';
+import React,{ Component} from "react"
+import './App.css';
+import {useDebugValue} from "react";
+
+function App() {
+    return <Say/>
+}
+
+export default App;
