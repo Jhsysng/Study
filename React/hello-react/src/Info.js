569f27bee36a678960b9a6d68f685e1600e6f2a5
Jhsysng <screenshot161@gmail.com>
2023-04-03 20:58:41 +0900

    use react

---
+++ React/hello-react/src/Info.js
@@ -0,0 +1,21 @@
+import {useState, useEffect} from "react";
+
+const Info=()=>{
+    const [name,setName]=useState('');
+    const [nickname,setNickname]=useState('');
+
+    useEffect(()=>{
+        console.log('렌더링이 완료되었습니다.');
+        console.log({
+            name,
+            nickname
+        });
+    })
+    const onChangeName=e=>{
+        setName(e.target.value);
+    };
+
+    const onChangeNickname=e=>{
+        setNickname(e.target.value);
+    };
+}
