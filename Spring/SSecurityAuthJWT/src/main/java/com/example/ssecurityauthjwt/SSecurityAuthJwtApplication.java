27b627cd362deb749d66291d879fadb0f0b907f8
Jhsysng <screenshot161@gmail.com>
2023-08-29 14:48:12 +0900

    auth server

---
+++ Spring/SSecurityAuthJWT/src/main/java/com/example/ssecurityauthjwt/SSecurityAuthJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssecurityauthjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSecurityAuthJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SSecurityAuthJwtApplication.class, args);
+    }
+
+}
---
+++ Spring/SSecurityAuthJWT/src/test/java/com/example/ssecurityauthjwt/SSecurityAuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssecurityauthjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SSecurityAuthJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
