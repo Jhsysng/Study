489cdfce781eac1945db8e89ec4fc7c69dfbed55
alex <alex@alexui-MacBookPro.local>
2024-02-05 18:42:19 +0900

    react, spring boot autoconfig

---
+++ React/react-todo-app/src/App.js
@@ -0,0 +1,49 @@
+import React, {Component} from "react";
+import "./App.css"
+export default class App extends Component{
+  btnStyle = {
+    color: "#fff",
+    border:"none",
+    padding:"5px 9px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float: "right"
+  };
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: 'none'
+    }
+  }
+  todoData = [
+    {
+      id:"1",
+      title:"공부하기",
+      complted: true
+    },
+    {
+      id: "2",
+      title:"청소하기",
+      complted: false
+    }
+  ]
+  render() {
+    return(
+      <div className = "container">
+        <div className="todoBlock">
+          <div className="title">
+          <h1>할 일 목록</h1>
+          </div>
+          {this.todoData.map( data => (          
+          <div style={this.getStyle()} key= {data.id}>
+            <input type="checkbox" defaultChecked={false}/>
+            {data.title}
+            <button style={this.btnStyle}>sx</button>
+          </div>))}
+
+        </div>
+      </div>
+    )
+  }
+} 
