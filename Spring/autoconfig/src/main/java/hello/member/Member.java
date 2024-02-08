a638966114cf4be0d69f664f2bdf61bad80b6eeb
alex <alex@alexui-MacBookPro.local>
2024-02-08 18:36:21 +0900

    react hook

--- Spring/autoconfig/src/main/java/hello/member/Member.java
+++ Spring/autoconfig/src/main/java/hello/member/Member.java
@@ -3,8 +3,13 @@ package hello.member;
 import lombok.Data;
 
 @Data
-public class Memeber{
+public class Member {
     private String memberId;
     private String name;
-    
+    public Member(){
+    }
+    public Member(String memberId, String name){
+        this.memberId = memberId;
+        this.name = name;
+    }
 }
