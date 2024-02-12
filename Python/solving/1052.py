621633089a0d533f34bb35ad15690940ce1b798a
Jhsysng <ysng1118@naver.com>
2024-02-12 21:19:32 +0900

    spring memrory auto library

---
+++ Python/solving/1052.py
@@ -0,0 +1,14 @@
+# 1052 python
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+count = 0
+
+while bin(n).count('1') > k:
+    n = n+1
+    count = count +1
+
+print(count)
