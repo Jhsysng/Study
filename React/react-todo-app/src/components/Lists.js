6ca04b690d97a9edce302b1a5273d8180ffd8560
Jhsysng <ysng1118@naver.com>
2024-02-29 22:56:10 +0900

    spring

--- React/react-todo-app/src/components/Lists.js
+++ React/react-todo-app/src/components/Lists.js
@@ -2,40 +2,22 @@ import React from 'react'
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import List from "./List";
 
-const Lists = React.memo(({todoData, setTodoData}) => {
+const Lists = React.memo(({todoData, setTodoData, handleClick}) => {
     const handleEnd = (result) => {
         const { source, destination } = result;
     
-        // 목적지가 없는 경우 (드래그했지만 같은 자리에 놓거나, 유효하지 않은 위치에 놓은 경우)
-        if (!destination) {
-            return;
-        }
+        if(!result.destination) return;
+        const newTodoData = todoData;
+        const [reorderItem] = newTodoData.splice(source.index, 1);
     
-        // 소스와 목적지의 인덱스가 같은 경우, 즉 위치가 변하지 않은 경우
-        if (
-            source.droppableId === destination.droppableId &&
-            source.index === destination.index
-        ) {
-            return;
-        }
-    
-        // 배열을 재배열하는 로직
-        const newTodoData = reorder(todoData, source.index, destination.index);
+        newTodoData.splice(destination.index, 0, reorderItem);
         setTodoData(newTodoData);
     };
-    
-    // 배열을 재배열하는 함수
-    const reorder = (list, startIndex, endIndex) => {
-        const result = Array.from(list);
-        const [removed] = result.splice(startIndex, 1);
-        result.splice(endIndex, 0, removed);
-    
-        return result;
-    };
+
   return (
     <div>
     <DragDropContext onDragEnd={handleEnd}>
-        <Droppable droppableId='todod'>
+        <Droppable droppableId='todo'>
             {(provided) => (
                 <div {...provided.droppableProps} ref={provided.innerRef}>
                     {todoData.map((data,index) => (          
@@ -46,6 +28,7 @@ const Lists = React.memo(({todoData, setTodoData}) => {
                     >
                         {(provided, snapshot) => (
                             <List
+                                handleClick={handleClick}
                                 key={data.id}
                                 id={data.id}
                                 title={data.title}
