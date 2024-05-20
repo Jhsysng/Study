6845f6953ce6c51df2fc921617b2d559e0cced5b
Jhsysng <ysng1118@naver.com>
2024-05-21 01:50:29 +0900

    entity

---
+++ Spring/JPAapp/src/main/java/study/jpashop/JpAappApplication.java
@@ -0,0 +1,14 @@
+package study.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpAappApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpAappApplication.class, args);
+    }
+
+}
