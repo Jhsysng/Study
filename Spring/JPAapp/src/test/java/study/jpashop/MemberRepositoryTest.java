44f26ae6789e178d95a7c794ad020b679aef47ab
Jhsysng <ysng1118@naver.com>
2024-05-25 01:24:55 +0900

    jpashop memberservice repository

--- Spring/JPAapp/src/test/java/study/jpashop/MemberRepositoryTest.java
+++ Spring/JPAapp/src/test/java/study/jpashop/MemberRepositoryTest.java
@@ -4,10 +4,10 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.jupiter.api.Assertions.*;
+import study.jpashop.domain.Member;
+import study.jpashop.repository.MemberRepository;
 
 @SpringBootTest
 class MemberRepositoryTest {
@@ -17,19 +17,21 @@ class MemberRepositoryTest {
 
     @Test
     @Transactional
+    @Rollback(false)
     public void testMember() throws Exception {
         // given
-        Member member = new Member();
-        member.setUsername("memberA");
-
-        // when
-        Long saveId = memberRepository.save(member);
-        Member findMember = memberRepository.find(saveId);
-
-        // then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        Assertions.assertThat(findMember).isEqualTo(member);
+//        Member member = new Member();
+//        member.setUsername("memberA");
+//
+//        // when
+//        Long saveId = memberRepository.save(member);
+//        Member findMember = memberRepository.find(saveId);
+//
+//        // then
+//        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+//        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+//        Assertions.assertThat(findMember).isEqualTo(member);
+//        System.out.println("findMember == member: " + (findMember == member));
     }
 
 }
