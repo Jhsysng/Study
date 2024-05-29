08e6362bb96c854f9887feda970119f662ae31fc
Jhsysng <ysng1118@naver.com>
2024-05-30 00:47:06 +0900

    jpa app domain

---
+++ Spring/JPAapp/src/main/java/study/jpashop/controller/HomeController.java
@@ -0,0 +1,15 @@
+package study.jpashop.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+    public String home() {
+        log.info("home controller");
+        return "home";
+    }
+
+}
