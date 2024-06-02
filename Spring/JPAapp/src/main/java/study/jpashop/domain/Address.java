d78df87e84fc677005303e09f7a2e1bdc8bf22c5
Jhsysng <ysng1118@naver.com>
2024-06-03 00:15:39 +0900

    controller

--- Spring/JPAapp/src/main/java/study/jpashop/domain/Address.java
+++ Spring/JPAapp/src/main/java/study/jpashop/domain/Address.java
@@ -2,12 +2,15 @@ package study.jpashop.domain;
 
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
-
     private String city;
     private String street;
     private String zipcode;
