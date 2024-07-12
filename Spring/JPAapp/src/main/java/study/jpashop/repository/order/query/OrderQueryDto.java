5e829f6c3bae1c36b36c4e2cc87c1f756f49b009
Jhsysng <ysng1118@naver.com>
2024-07-13 01:16:21 +0900

    v3.1

---
+++ Spring/JPAapp/src/main/java/study/jpashop/repository/order/query/OrderQueryDto.java
@@ -0,0 +1,35 @@
+package study.jpashop.repository.order.query;
+
+import lombok.Data;
+import study.jpashop.domain.Address;
+import study.jpashop.domain.OrderStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class OrderQueryDto {
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+    private List<OrderItemQueryDto> orderItems;
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
+    }
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+    }
+}
