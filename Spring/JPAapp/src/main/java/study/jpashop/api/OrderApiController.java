beefd669b431c3f00b24a87baaca3985595f29cb
Jhsysng <ysng1118@naver.com>
2024-07-14 00:13:10 +0900

    v5

--- Spring/JPAapp/src/main/java/study/jpashop/api/OrderApiController.java
+++ Spring/JPAapp/src/main/java/study/jpashop/api/OrderApiController.java
@@ -71,6 +71,16 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
+    @GetMapping("/api/v6/orders")
+    public List<OrderQueryDto> ordersV6() {
+        return orderQueryRepository.findAllByDto_flat();
+    }
+
 
     @Getter
     static class OrderDto {
