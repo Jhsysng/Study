2ab3117dfa055073666ba3ed2a4d88562ef261e3
Jhsysng <ysng1118@naver.com>
2024-07-20 02:04:43 +0900

    thymeleaf 2/3

--- Spring/thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ Spring/thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -1,6 +1,8 @@
 package hello.thymeleafbasic.basic;
 
 import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/basic")
@@ -51,10 +55,64 @@ public class BasicController {
         return "basic/operation";
     }
 
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
+
+    @GetMapping("/each")
+    public String each(Model model){
+        addUser(model);
+
+        return "basic/each";
+    }
+
+    @GetMapping("/condition")
+    public String condtion(Model model){
+        addUser(model);
+        return "basic/condition";
+    }
+
+    @GetMapping("/comments")
+    public String commets(Model model){
+        return "basic/comments";
+    }
+
+    @GetMapping("/block")
+    public String block(Model model){
+        addUser(model);
+
+        return "basic/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model){
+        model.addAttribute("user", new User("UserA", 10));
+        addUser(model);
+
+        return "basic/javascript";
+    }
+
+    private void addUser(Model model){
+        List<User> list = new ArrayList<>();
+        list.add(new User("UserA", 10));
+        list.add(new User("UserB", 20));
+        list.add(new User("UserC", 30));
+
+        model.addAttribute("users", list);
+    }
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
             return "Hello " + data;
         }
+    }
+
+    @Data
+    @AllArgsConstructor
+    public class User {
+        private String username;
+        private int age;
     }
 }
