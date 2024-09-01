693a8ceba9cecfbafadeebc54c89b86e3ba1b922
Jhsysng <ysng1118@naver.com>
2024-09-02 00:54:31 +0900

    loginterceptor

---
+++ Spring/login/src/main/java/hello/login/web/interceptor/LogInterceptor.java
@@ -0,0 +1,47 @@
+package hello.login.web.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.UUID;
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+
+    public static final String LOGIN_ID = "loginId";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String requestURI = request.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+
+        request.setAttribute(LOGIN_ID, uuid);
+
+        log.info("REQUEST [{}][{}][{}]", uuid, requestURI, handler);
+
+        if(handler instanceof HandlerMethod){
+            HandlerMethod hm = (HandlerMethod) handler;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("postHandle modelAndView={}", modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        String requestURI = request.getRequestURI();
+        Object loginId = request.getAttribute(LOGIN_ID);
+
+        log.info("RESPONSE [{}][{}][{}]", loginId, requestURI, handler);
+
+    }
+}
