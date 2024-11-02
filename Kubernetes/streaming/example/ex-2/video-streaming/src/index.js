0ae6759205fc15b3223b95e3485377f1ab26ba83
Jhsysng <ysng1118@naver.com>
2024-11-02 09:54:49 +0900

    rabbit exchange

--- Kubernetes/streaming/example/ex-2/video-streaming/src/index.js
+++ Kubernetes/streaming/example/ex-2/video-streaming/src/index.js
@@ -1,73 +1,93 @@
-const express = require('express');
-const { MongoClient, ObjectId } = require('mongodb');
-const http = require('http');
+const express = require("express");
+const fs = require("fs");
+const amqp = require('amqplib');
 
-const app = express();
+if (!process.env.RABBIT) {
+    throw new Error("Please specify the name of the RabbitMQ host using environment variable RABBIT");
+}
 
-const PORT = process.env.PORT || 3000;
-const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST || 'localhost';
-const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT) || 4000;
-const DBHOST = process.env.DBHOST || 'mongodb://localhost:27017';
-const DBNAME = process.env.DBNAME || 'video-streaming';
+const RABBIT = process.env.RABBIT;
 
+//
+// Connect to the RabbitMQ server.
+//
+function connectRabbit() {
+
+    console.log(`Connecting to RabbitMQ server at ${RABBIT}.`);
+
+    return amqp.connect(RABBIT) // Connect to the RabbitMQ server.
+        .then(connection => {
+            console.log("Connected to RabbitMQ.");
+
+            return connection.createChannel(); // Create a RabbitMQ messaging channel.
+        });
+}
+
+//
+// Send the "viewed" to the history microservice.
+//
+function sendViewedMessage(messageChannel, videoPath) {
+    console.log(`Publishing message on "viewed" queue.`);
+
+    const msg = { videoPath: videoPath };
+    const jsonMsg = JSON.stringify(msg);
+    messageChannel.publish("", "viewed", Buffer.from(jsonMsg)); // Publish message to the "viewed" queue.
+}
+
+//
+// Setup event handlers.
+//
+function setupHandlers(app, messageChannel) {
+    app.get("/video", (req, res) => { // Route for streaming video.
+
+        const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
+        fs.stat(videoPath, (err, stats) => {
+            if (err) {
+                console.error("An error occurred ");
+                res.sendStatus(500);
+                return;
+            }
+    
+            res.writeHead(200, {
+                "Content-Length": stats.size,
+                "Content-Type": "video/mp4",
+            });
+    
+            fs.createReadStream(videoPath).pipe(res);
+
+            sendViewedMessage(messageChannel, videoPath); // Send message to "history" microservice that this video has been "viewed".
+        });
+    });
+}
+
+//
+// Start the HTTP server.
+//
+function startHttpServer(messageChannel) {
+    return new Promise(resolve => { // Wrap in a promise so we can be notified when the server has started.
+        const app = express();
+        setupHandlers(app, messageChannel);
+
+        const port = process.env.PORT && parseInt(process.env.PORT) || 3000;
+        app.listen(port, () => {
+            resolve(); // HTTP server is listening, resolve the promise.
+        });
+    });
+}
+
+//
+// Application entry point.
+//
 function main() {
-    console.log('Attempting to connect to MongoDB at:', DBHOST);
-    return MongoClient.connect(DBHOST, { useUnifiedTopology: true })
-        .then(client => {
-            console.log('Connected successfully to MongoDB');
-            const db = client.db(DBNAME);
-            const videosCollection = db.collection('videos');
-
-            app.get("/video", (req, res) => {
-                let videoId;
-                try {
-                    videoId = new ObjectId(req.query.id);
-                } catch (error) {
-                    res.status(400).send("Invalid video ID");
-                    return;
-                }
-
-                videosCollection.findOne({ _id: videoId })
-                    .then(videoRecord => {
-                        if (!videoRecord) {
-                            res.status(404).send("Video not found");
-                            return;
-                        }
-                        console.log('Forwarding request to video storage');
-                        const forwardRequest = http.request({
-                            host: VIDEO_STORAGE_HOST,
-                            port: VIDEO_STORAGE_PORT,
-                            path: `/video?path=${videoRecord.videoPath}`,
-                            method: 'GET',
-                            headers: req.headers
-                        },
-                        forwardResponse => {
-                            res.writeHead(forwardResponse.statusCode, forwardResponse.headers);
-                            forwardResponse.pipe(res);
-                        });
-                        forwardRequest.on('error', (err) => {
-                            console.error('Error forwarding request to video storage:', err);
-                            res.status(500).send('Error retrieving video from storage');
-                        });
-                        req.pipe(forwardRequest);
-                    })
-                    .catch(err => {
-                        console.error("Database query failed:", err);
-                        res.status(500).send("Internal server error");
-                    });
-            });
-
-            app.listen(PORT, () => {
-                console.log(`Server is running on http://localhost:${PORT}`);
-            });
+    return connectRabbit()                          // Connect to RabbitMQ...
+        .then(messageChannel => {                   // then...
+            return startHttpServer(messageChannel); // start the HTTP server.
         });
 }
 
 main()
-    .then(() => {
-        console.log('Server started successfully');
-    })
+    .then(() => console.log("Microservice online."))
     .catch(err => {
-        console.error('Failed to start the server:', err);
-        process.exit(1);
+        console.error("Microservice failed to start.");
+        console.error(err && err.stack || err);
     });
