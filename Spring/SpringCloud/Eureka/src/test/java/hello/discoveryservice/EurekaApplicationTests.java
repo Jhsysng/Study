e31f4ad9bc054adf590de197f0530b75b9165a06
Jhsysng <ysng1118@naver.com>
2024-12-29 00:00:01 +0900

    spring cloud eureka, userservice add

---
+++ Spring/SpringCloud/Eureka/src/test/java/hello/discoveryservice/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
