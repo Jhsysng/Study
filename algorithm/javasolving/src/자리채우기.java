c4f92674af1edf499fcfee4d5bcdbb339b326125
Jhsysng <ysng1118@naver.com>
2025-01-09 14:28:01 +0900

    자리채우기

--- algorithm/javasolving/src/자리채우기.java
+++ algorithm/javasolving/src/자리채우기.java
@@ -3,23 +3,41 @@ import java.io.InputStreamReader;
 
 public class 자리채우기 {
     private static final int[] dx = {0, 1, 0, -1};
-    private static final int[] dy = {-1, 0, 1, 0};
+    private static final int[] dy = {1, 0, -1, 0};
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] s = br.readLine().split(" ");
-        int[] m = new int[2];
-        for(int i = 0 ; i< 2; i++){
-            m[i] = Integer.parseInt(s[i]);
-        }
+        int C = Integer.parseInt(s[0]);
+        int R = Integer.parseInt(s[1]);
+        int K = Integer.parseInt(br.readLine());
 
-        int k = Integer.parseInt(br.readLine());
-
-        if(m[0]*m[1] < k){
+        if (K > C * R) {
             System.out.println(0);
             return;
         }
 
-        int[][] matrix = new int[m[1]][m[0]];
+        int x = 1, y = 1;
+        int dir = 0;
+        int count = 1;
+        boolean[][] visited = new boolean[R + 1][C + 1];
 
+        while (count < K) {
+            visited[y][x] = true;
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+
+            if (nx < 1 || nx > C || ny < 1 || ny > R || visited[ny][nx]) {
+                dir = (dir + 1) % 4;
+                nx = x + dx[dir];
+                ny = y + dy[dir];
+            }
+
+            x = nx;
+            y = ny;
+            count++;
+        }
+
+        System.out.println(x + " " + y);
     }
 }
